d2c642768e3195344b863e1d9c7d30dc12978bac
nicko <nickozeusfagarin@gmail.com>
2024-10-19 16:44:31 +0800

    sample second  commit

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $config['base_url'] = 'http://localhost/dwcc-sms/';
 //$config['base_url'] = 'http://192.168.0.3/dwcc-sms/';
-
+//sample
 /*
 |--------------------------------------------------------------------------
 | Index File
