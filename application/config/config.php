4efd4efc8f7bfd21cdc8016160c758271941e51c
nicko <nickozeusfagarin@gmail.com>
2024-10-19 16:32:15 +0800

    Sample

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'http://localhost/dwcc-sms/';
-$config['base_url'] = 'http://192.168.0.3/dwcc-sms/';
+$config['base_url'] = 'http://localhost/dwcc-sms/';
+//$config['base_url'] = 'http://192.168.0.3/dwcc-sms/';
 
 /*
 |--------------------------------------------------------------------------
